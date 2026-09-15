1f3af09210d1cc020a16fecf255d3b019f9f4766
yeyen1meilani-lgtm <yeyen1meilani@gmail.com>
2026-09-15 12:53:03 +0700

    Initial project

---
+++ main.dart
@@ -0,0 +1,5 @@
+void main() {
+    print("Hello World!") ;
+    print("Saya yeyen sri meilani, senang belajar dart flutter") ;
+
+}
---
+++ sistem_bilangan.dart
@@ -0,0 +1,12 @@
+void main() {
+  int decimal = 10;
+  int octal = int.parse('12', radix: 8);
+  int binary = int.parse('1010', radix: 2);
+  int hexadecimal = 0xA;
+  
+  print("Sistem Bilangan Yeyen Sri Meilani");
+  print(decimal);
+  print(binary);
+  print(hexadecimal);
+  print(octal);
+}
